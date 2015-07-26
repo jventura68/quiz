56f93dc157609dd2ab0bfaad7d820e88165bdd49
Jose Ventura <jose.ventura.roda@gmail.com>
2015-07-26 20:00:16 +0200

    Autoload y gestión de errores

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,28 +1,37 @@
 var express = require('express');
 var models = require ('../models/models.js');
 
-//GET quizes
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function (req, res, next, quizId){
+  models.Quiz.find(quizId).then(
+    function(quiz){
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      }else {
+        next (new Error ('No existe quizId='+quizId));
+      }
+    }
+  ).catch(function(error) {next(error);});
+};
 
+//GET quizes
 exports.index = function (req,res) {
     models.Quiz.findAll().then( function (quizes) {
         res.render('quizes/index', {quizes:quizes});
-    })
+    }).catch(function(error) {next(error);});
 }
 
 //GET /quizes/:quizID
 exports.show = function (req,res) {
-    models.Quiz.find(req.params.quizId).then(function(quiz) {
-        res.render('quizes/show',{quiz: quiz});
-    })
+    res.render('quizes/show',{quiz: req.quiz});
 };
 
 //GET /quizes/answer
 exports.answer = function (req,res) {
-    models.Quiz.find(req.params.quizId).then(function(quiz) {
-      if (req.query.respuesta === quiz.respuesta) {
-        res.render ('quizes/answer', {respuesta: 'Correcto', quiz: quiz});
-      }else {
-        res.render ('quizes/answer', {respuesta: 'Incorrecto', quiz: quiz});
-      }
-    })
+    var resultado = 'Incorrecto';
+    if (req.query.respuesta === req.quiz.respuesta) {
+        resultado = 'Correcto';
+    }
+    res.render ('quizes/answer', {respuesta: resultado, quiz: req.quiz});
 };
--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,15 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param ('quizId', quizController.load);
+
+// Definición de rutas de /quizes
 router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
+
+// Definición de rutas de author
 router.get('/author',                       authorController.author);
 
 module.exports = router;
