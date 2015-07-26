eaace02a9047728b9b6a72bb5601deab51281687
Jose Ventura <jose.ventura.roda@gmail.com>
2015-07-26 22:05:52 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,11 @@ router.get('/', function(req, res) {
 router.param ('quizId', quizController.load);
 
 // Definición de rutas de /quizes
-router.get('/quizes',                       quizController.index);
-router.get('/quizes/:quizId(\\d+)',         quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
+router.get ('/quizes',                       quizController.index);
+router.get ('/quizes/:quizId(\\d+)',         quizController.show);
+router.get ('/quizes/:quizId(\\d+)/answer',  quizController.answer);
+router.get ('/quizes/new',                   quizController.new);
+router.post('/quizes/create',                quizController.create);
 
 // Definición de rutas de author
 router.get('/author',                       authorController.author);
