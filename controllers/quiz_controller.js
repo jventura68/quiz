5f2816f0733090146750b0373a3ff8b7a3362284
Jose Ventura <jose.ventura.roda@gmail.com>
2015-07-28 20:02:30 +0200

    Validar create

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -25,13 +25,13 @@ exports.index = function (req,res) {
       where = {where: {pregunta:{like:search}}};
     }
     models.Quiz.findAll(where).then( function (quizes) {
-        res.render('quizes/index', {quizes:quizes});
+        res.render('quizes/index', {quizes:quizes,errors: []});
     }).catch(function(error) {next(error);});
 }
 
 //GET /quizes/:quizID
 exports.show = function (req,res) {
-    res.render('quizes/show',{quiz: req.quiz});
+    res.render('quizes/show',{quiz: req.quiz,errors: []});
 };
 
 //GET /quizes/answer
@@ -40,7 +40,7 @@ exports.answer = function (req,res) {
     if (req.query.respuesta === req.quiz.respuesta) {
         resultado = 'Correcto';
     }
-    res.render ('quizes/answer', {respuesta: resultado, quiz: req.quiz});
+    res.render ('quizes/answer', {respuesta: resultado, quiz: req.quiz, errors: []});
 };
 
 //GET /quizes/new
@@ -48,15 +48,25 @@ exports.new= function (req,res) {
     var quiz = models.Quiz.build(
         {pregunta: 'Pregunta', respuesta:'Respuesta'}
     );
-    res.render ('quizes/new', {quiz: quiz});
+    res.render ('quizes/new', {quiz: quiz, errors: []});
 };
 
 
 //POST /quizes/create
-exports.create = function (req,res) {
-    var quiz= models.Quiz.build(req.body.quiz);
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build(req.body.quiz);
+
+  var errors = quiz.validate();
+  if (errors) {
+    var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+    for (var prop in errors) errores[i++]={message: errors[prop]};
+    res.render('quizes/new', {quiz: quiz, errors: errores});
+  } else {
     //guarda en DB los campos pregunta y respuesta de quiz
-    quiz.save ({fields: ["pregunta", "respuesta"]}).then (function() {
-        res.redirect ('/quizes');
-    })
+    quiz.save({
+      fields: ["pregunta", "respuesta"]
+    }).then(function() {
+      res.redirect('/quizes');
+    });
+  }
 };
